1bc45beb962cdb3a2f0b8f5e7f0244527e65dff9
kayjeee <kagiso.killagram@gmail.com>
2023-04-28 07:58:18 +0200

    fixed linters

--- index.js
+++ index.js
@@ -1,61 +1,61 @@
-// Function to add a book to the collection
-function addBook(title, author) {
-  const newBook = { title, author };
-  bookCollection.push(newBook);
-  localStorage.setItem("bookCollection", JSON.stringify(bookCollection));
-  renderBookList();
+const books = JSON.parse(localStorage.getItem('form')) || [];
+
+function displayBooks(book, index) {
+  const content = `
+    <div>
+        <p>${book.title}</p>
+        <p>${book.author}</p>    
+        <button id ="delete${index}">Remove</button>
+        <hr>
+    </div>
+    `;
+  return content;
 }
 
-// Function to remove a book from the collection
-function removeBook(title, author) {
-  bookCollection = bookCollection.filter(
-    (book) => book.title !== title || book.author !== author
-  );
-  localStorage.setItem("bookCollection", JSON.stringify(bookCollection));
-  renderBookList();
+function storeForm(form) {
+  localStorage.setItem('form', JSON.stringify(form));
 }
 
-// Function to render the book list
-function renderBookList() {
-  bookList.innerHTML = "";
-  bookCollection.forEach((book) => {
-    const li = document.createElement("li");
-    const title = document.createElement("span");
-    title.textContent = book.title;
-    const author = document.createElement("span");
-    author.textContent = ` by ${book.author}`;
-    const removeBtn = document.createElement("button");
-    removeBtn.textContent = "Remove";
-    removeBtn.addEventListener("click", () => {
-      removeBook(book.title, book.author);
-    });
-    li.appendChild(title);
-    li.appendChild(author);
-    li.appendChild(removeBtn);
-    bookList.appendChild(li);
-  });
-}
+const addButton = document.getElementById('addBook');
+const newTitle = document.getElementById('bookTitle');
+const newAuthor = document.getElementById('authorName');
+const container = document.querySelector('.bodyh1');
 
-// Retrieve saved book data from localStorage (if any)
-let bookCollection = JSON.parse(localStorage.getItem("bookCollection")) || [];
+addButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  if (newAuthor.value !== '' && newTitle.value !== '') {
+    const title = newTitle.value;
+    const author = newAuthor.value;
+    const newBook = {
+      title,
+      author,
+    };
 
-// Select DOM elements
-const titleInput = document.getElementById("title");
-const authorInput = document.getElementById("author");
-const addBtn = document.getElementById("add-btn");
-const bookList = document.getElementById("book-list");
-
-// Display initial book collection
-renderBookList();
-
-// Add event listener to "Add" button
-addBtn.addEventListener("click", (event) => {
-  event.preventDefault();
-  const title = titleInput.value.trim();
-  const author = authorInput.value.trim();
-  if (title && author) {
-    addBook(title, author);
-    titleInput.value = "";
-    authorInput.value = "";
+    books.push(newBook);
+    storeForm(books);
   }
+  addButton.form.reset(); // use form property of the button element to reset the form
+});
+
+for (let i = 0; i < books.length; i += 1) {
+  const book = displayBooks(books[i], i);
+  container.innerHTML += book;
+}
+
+const deleteBtn = document.querySelectorAll('[id^="delete"]');
+deleteBtn.forEach((button) => {
+  button.addEventListener('click', () => {
+    const item = button.parentNode;
+    item.parentNode.removeChild(item);
+
+    const { id } = button;
+
+    for (let i = 0; i < books.length; i += 1) {
+      if (id.includes(i)) {
+        books.splice(i, 1);
+      }
+    }
+
+    storeForm(books);
+  });
 });
