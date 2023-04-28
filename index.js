0d04de7b90020aed8a05550d70793bc9dd865a31
kayjeee <kagiso.killagram@gmail.com>
2023-04-28 07:17:45 +0200

    fixed linters

--- index.js
+++ index.js
@@ -1,43 +1,61 @@
-const bookList = [];
+// Retrieve saved book data from localStorage (if any)
+let bookCollection = JSON.parse(localStorage.getItem('bookCollection')) || [];
 
-function renderBookList() {
-  const bookListElement = document.getElementById('bookList');
-  bookListElement.innerHTML = '';
+// Select DOM elements
+const titleInput = document.getElementById('title');
+const authorInput = document.getElementById('author');
+const addBtn = document.getElementById('add-btn');
+const bookList = document.getElementById('book-list');
 
-  bookList.forEach((book, index) => {
-    const bookElement = document.createElement('div');
-    bookElement.innerHTML = `
-      <h2>${book.title}</h2>
-      <p>Author: ${book.author}</p>
-      <button class="removeButton" data-index="${index}">Remove</button>
-    `;
-    bookListElement.appendChild(bookElement);
-  });
-}
+// Display initial book collection
+renderBookList();
 
-function addBook() {
-  const titleInput = document.getElementById('titleInput');
-  const authorInput = document.getElementById('authorInput');
-  const title = titleInput.value;
-  const author = authorInput.value;
+// Add event listener to "Add" button
+addBtn.addEventListener('click', (event) => {
+  event.preventDefault();
+  const title = titleInput.value.trim();
+  const author = authorInput.value.trim();
   if (title && author) {
-    bookList.push({ title, author });
-    renderBookList();
+    addBook(title, author);
     titleInput.value = '';
     authorInput.value = '';
   }
-}
-
-const addButton = document.getElementById('addButton');
-addButton.addEventListener('click', addBook);
-
-const bookListElement = document.getElementById('bookList');
-bookListElement.addEventListener('click', (event) => {
-  if (event.target.classList.contains('removeButton')) {
-    const index = event.target.getAttribute('data-index');
-    bookList.splice(index, 1);
-    renderBookList();
-  }
 });
 
-renderBookList();
+// Function to add a book to the collection
+function addBook(title, author) {
+  const newBook = { title, author };
+  bookCollection.push(newBook);
+  localStorage.setItem('bookCollection', JSON.stringify(bookCollection));
+  renderBookList();
+}
+
+// Function to remove a book from the collection
+function removeBook(title, author) {
+  bookCollection = bookCollection.filter(
+    (book) => book.title !== title || book.author !== author,
+  );
+  localStorage.setItem('bookCollection', JSON.stringify(bookCollection));
+  renderBookList();
+}
+
+// Function to render the book list
+function renderBookList() {
+  bookList.innerHTML = '';
+  bookCollection.forEach((book) => {
+    const li = document.createElement('li');
+    const title = document.createElement('span');
+    title.textContent = book.title;
+    const author = document.createElement('span');
+    author.textContent = ` by ${book.author}`;
+    const removeBtn = document.createElement('button');
+    removeBtn.textContent = 'Remove';
+    removeBtn.addEventListener('click', () => {
+      removeBook(book.title, book.author);
+    });
+    li.appendChild(title);
+    li.appendChild(author);
+    li.appendChild(removeBtn);
+    bookList.appendChild(li);
+  });
+}
